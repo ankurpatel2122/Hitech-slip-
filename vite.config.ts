4b3e00db5118856b56cf44666e09ad6b224c8803
ankurpatel2122 <luckyno2122@gmail.com>
2025-06-14 21:05:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.AIzaSyAaLIV7OGol-XvcuieKOXwRT--thR19Eps),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyAaLIV7OGol-XvcuieKOXwRT--thR19Eps)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
